fe49fcfac7eddfbe7bd5741c36eba28fdc818ec5
Ashron Betha <107986548+ashronashle@users.noreply.github.com>
2024-07-11 22:23:57 +0200

    Add files via upload

---
+++ Pinterest/javaScript.js
@@ -0,0 +1,338 @@
+const imageData = [
+  {
+    id: "9b24be2c-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://www.wallpaperflare.com/static/335/587/630/blooming-lotus-flower-selective-focus-photography-wallpaper.jpg",
+    imageProfileImage:
+      "https://www.wallpaperflare.com/static/335/587/630/blooming-lotus-flower-selective-focus-photography-wallpaper.jpg",
+    profileName: "Sarah",
+    suggestedCategory: "Flower",
+    title: "Blooming locus",
+  },
+  {
+    id: "9b24c6d4-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/OIP.08yJatiiPWLnBV5OZBscigHaHa?rs=1&pid=ImgDetMain",
+    imageProfileImage:
+      "https://thumbs.dreamstime.com/b/young-man-face-cartoon-vector-illustration-graphic-design-man-face-cartoon-144449592.jpg",
+    profileName: "Coding Master",
+    suggestedCategory: "Code",
+    title: "",
+  },
+  {
+    id: "9b24c7c0-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/OIP.cJ1qXXAEum7sTQHFIfPtrQHaE8?rs=1&pid=ImgDetMain",
+    imageProfileImage:
+      "https://cdn4.vectorstock.com/i/1000x1000/46/28/boy-face-cartoon-vector-18204628.jpg",
+    profileName: "Daniel",
+    suggestedCategory: "Travel",
+    title: "",
+  },
+  {
+    id: "9b24c8a0-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/OIP.lCQZdPU9YAD6bZmdGc2nVwAAAA?rs=1&pid=ImgDetMain",
+    imageProfileImage:
+      "https://static.vecteezy.com/system/resources/previews/000/662/907/original/vector-man-face-cartoon.jpg",
+    profileName: "Peter",
+    suggestedCategory: "Code",
+    title: "",
+  },
+  {
+    id: "9b24c976-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/R.839bced65c46ba70b89cd5b3a16b8313?rik=yPLNHuMoHTqgcw&pid=ImgRaw&r=0",
+    imageProfileImage:
+      "https://cdn1.vectorstock.com/i/1000x1000/27/65/cute-boy-face-cartoon-vector-23302765.jpg",
+    profileName: "James",
+    suggestedCategory: "Fashion",
+    title: "street wear",
+  },
+  {
+    id: "9b24ca52-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/OIP.i2zI7vDSAhmyBpO5jt1AQwHaFj?rs=1&pid=ImgDetMain",
+    imageProfileImage:
+      "https://th.bing.com/th/id/OIP.a5xrp38s2EKOVCQfJc2aBQHaHa?rs=1&pid=ImgDetMain",
+    profileName: "Olivia",
+    suggestedCategory: "Flower",
+    title: "Flower|Rainy",
+  },
+  {
+    id: "9b24cb28-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/OIP.LLATJqJS8oj-YtHXYmDSrQHaHa?w=736&h=736&rs=1&pid=ImgDetMain",
+    imageProfileImage:
+      "https://th.bing.com/th/id/OIP.SLZTuWCUtC6iPS55iuAEgQHaI9?w=892&h=1080&rs=1&pid=ImgDetMain",
+    profileName: "liam",
+    suggestedCategory: "Scripture",
+    title: "",
+  },
+  {
+    id: "9b24cc00-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/OIP.rD1_kh4By7iKwIjPq8_Y7AHaEK?rs=1&pid=ImgDetMain",
+    imageProfileImage:
+      "https://th.bing.com/th/id/R.0dfdad70a60634bbcef55d4d2ac66531?rik=OZo9gIXYAEWCvQ&pid=ImgRaw&r=0",
+    profileName: "Mia",
+    suggestedCategory: "Hair",
+    title: "",
+  },
+  {
+    id: "9b24ccd6-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://i.pinimg.com/originals/77/e0/7a/77e07aca0b465181bdf781ad3b7adcc9.jpg",
+    imageProfileImage:
+      "https://th.bing.com/th/id/R.4e5dffd441de723e86c5a9b05e9becf4?rik=uQBFl6m2tEqnHw&pid=ImgRaw&r=0",
+    profileName: "Noah",
+    suggestedCategory: "Fashion",
+    title: "",
+  },
+  {
+    id: "9b24cdb2-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://raw.githubusercontent.com/kufii/CodeSnap/master/examples/monokai_fira-code.png",
+    imageProfileImage:
+      "https://th.bing.com/th/id/R.4e5dffd441de723e86c5a9b05e9becf4?rik=uQBFl6m2tEqnHw&pid=ImgRaw&r=0",
+    profileName: "Ava",
+    suggestedCategory: "Code",
+    title: "",
+  },
+  {
+    id: "9b24ce8e-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://www.thespruce.com/thmb/otgP0XgxzKEdQGoujEolfo-41gQ=/2121x1414/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-677847736-591d09745f9b58f4c06d73a3.jpg",
+    imageProfileImage:
+      "https://i.pinimg.com/originals/9c/02/2e/9c022e84daae928d53f908f01ad1f038.jpg",
+    profileName: "Luca",
+    suggestedCategory: "Flower",
+    title: "",
+  },
+  {
+    id: "9b24cf6a-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://i.pinimg.com/736x/64/3e/51/643e517287f8e6c58368ebdb971d6dd5.jpg",
+    imageProfileImage:
+      "https://i.pinimg.com/originals/3a/17/7d/3a177dc3530e43e568a3183977229ad1.jpg",
+    profileName: "Isabella",
+    suggestedCategory: "Hair",
+    title: "",
+  },
+  {
+    id: "9b24d046-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/R.8cb20df9767e355b48dd494846c0c387?rik=d%2b5rBymbOzRB5Q&pid=ImgRaw&r=0",
+    imageProfileImage:
+      "https://th.bing.com/th/id/OIP.rSXaCkAqgZtUQXn8QCpSOwHaIp?rs=1&pid=ImgDetMain",
+    profileName: "Benjamin",
+    suggestedCategory: "Scripture",
+    title: "",
+  },
+  {
+    id: "9b24d122-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/OIP.l5sLDNSaXwczn7yzdNFdHgHaLG?rs=1&pid=ImgDetMain",
+    imageProfileImage:
+      "https://th.bing.com/th/id/R.f51caa43a807dc4c6be6c03acb64d7b8?rik=893XjWrJw58jOw&pid=ImgRaw&r=0",
+    profileName: "Alexander",
+    suggestedCategory: "Fashion",
+    title: "",
+  },
+  {
+    id: "9b24d1fe-1f0a-11ee-be56-0242ac120002",
+    imageUrl: "https://wallpaperaccess.com/full/948855.jpg",
+    imageProfileImage:
+      "https://th.bing.com/th/id/OIP.4ru-yCQe_dPZ65W0X3fcswHaE8?rs=1&pid=ImgDetMain",
+    profileName: "Charlotte",
+    suggestedCategory: "Scripture",
+    title: "",
+  },
+  {
+    id: "9b24d2da-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://www.museeinformatique.fr/wp-content/uploads/2022/11/code-source.jpg",
+    imageProfileImage:
+      "https://th.bing.com/th/id/OIP.Cs-_NPoa5gecqveL8RFwEAHaHa?rs=1&pid=ImgDetMain",
+    profileName: "Amelia",
+    suggestedCategory: "Code",
+    title: "",
+  },
+  {
+    id: "9b24d3b6-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/R.4b09b067a814c117ec1a34bbb021a00e?rik=Mytxyc8rtpBNIA&riu=http%3a%2f%2fwww.hdwallpapers.in%2fwalls%2fpurple_flowers-normal.jpg&ehk=1LFwUfNeO0jPoxDXFaA5LDa%2fU8JQPBSRMtZtI0VTzGI%3d&risl=&pid=ImgRaw&r=0",
+    imageProfileImage:
+      "https://th.bing.com/th/id/OIP.0_SKZH-jCNY0CDRFMzvG_gAAAA?rs=1&pid=ImgDetMain",
+    profileName: "Henry",
+    suggestedCategory: "Flower",
+    title: "",
+  },
+  {
+    id: "9b24d492-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/OIP.WoJ9MHMDigFeQ_TPcf7C1AHaJO?rs=1&pid=ImgDetMain",
+    imageProfileImage:
+      "https://th.bing.com/th/id/R.e4310c6599a49279e4ee34a30d9c7f72?rik=a%2f976wPKpk%2bo5Q&pid=ImgRaw&r=0",
+    profileName: "Harper",
+    suggestedCategory: "Hair",
+    title: "",
+  },
+  {
+    id: "9b24d56e-1f0a-11ee-be56-0242ac120002",
+    imageUrl:
+      "https://th.bing.com/th/id/R.a134b3aba2cd012fb77bfc1d1ae27f07?rik=H9JQPjafcrrQVA&pid=ImgRaw&r=0",
+    imageProfileImage:
+      "https://th.bing.com/th/id/OIP.0_SKZH-jCNY0CDRFMzvG_gAAAA?rs=1&pid=ImgDetMain",
+    profileName: "Elijah",
+    suggestedCategory: "Fashion",
+    title: "",
+  },
+  {
+    id: "9b24d64a-1f0a-11ee-be56-0242ac120002",
+    imageUrl: "https://images4.alphacoders.com/935/935588.jpg",
+    imageProfileImage:
+      "https://i1.wp.com/www.edwud.com/wp-content/uploads/2017/05/wild_daffodil_flowers_durham_england.jpg?fit=2000%2C1339&ssl=1",
+    profileName: "Flowers",
+    suggestedCategory: "Flower",
+    title: "",
+  },
+];
+
+function createImageCards() {
+  const container = document.getElementById("pinContainer");
+  container.innerHTML = "";
+
+  imageData.forEach((image, index) => {
+    const card = document.createElement("div");
+
+    if (index % 3 === 0) {
+      card.className = "card card_small";
+    } else if (index % 3 === 1) {
+      card.className = "card card_medium";
+    } else {
+      card.className = "card card_large";
+    }
+
+    const img = document.createElement("img");
+    img.src = image.imageUrl;
+    img.alt = image.profileName;
+
+    const overlay = document.createElement("div");
+    overlay.className = "overlay";
+
+    // Create save button
+    const saveBtn = document.createElement("button");
+    saveBtn.className = "save-btn";
+    saveBtn.textContent = "Save";
+
+    const dropdown = document.createElement("div");
+    dropdown.className = "dropdown";
+
+    const dropdownBtn = document.createElement("h4");
+    dropdownBtn.className = "dropdown-btn";
+    dropdownBtn.innerHTML = 'Fashion <i class="fas fa-caret-down"></i>';
+
+    const dropdownContent = document.createElement("div");
+    dropdownContent.className = "dropdown-content";
+
+    const categoryOption = document.createElement("a");
+    categoryOption.href = "#";
+    categoryOption.textContent = image.suggestedCategory;
+
+    dropdownContent.appendChild(categoryOption);
+    dropdown.appendChild(dropdownBtn);
+    dropdown.appendChild(dropdownContent);
+    const downloadMore = document.createElement("div");
+    downloadMore.className = "downloadMore";
+
+    const saveDrop = document.createElement("div");
+    saveDrop.className = "saveDrop";
+
+    const catagoryIconsave = document.createElement("div");
+    catagoryIconsave.className = "catagoryIconsave";
+
+    // Create download button
+    const downloadBtn = document.createElement("button");
+    downloadBtn.className = "download-btn";
+    downloadBtn.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(7, 7, 7, 1);transform: ;msFilter:;"><path d="M11 15h2V9h3l-4-5-4 5h3z"></path><path d="M20 18H4v-7H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-2v7z"></path></svg>';
+
+    const moreBtn = document.createElement("button");
+    moreBtn.className = "more-btn";
+    moreBtn.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(7, 7, 7, 1);transform: ;msFilter:;"><path d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zM6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path></svg>';
+   
+    const category = document.createElement("h4");
+    category.className = "categoryOver";
+    category.textContent = image.suggestedCategory;
+
+    const categoryicon = document.createElement("h4");
+    categoryicon.className = "categoryOver";
+    categoryicon.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(244, 242, 242, 1);transform: ;msFilter:;"><path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path></svg>'
+    /*const categoryContainer = document.createElement("div");
+    categoryContainer.className = "catagory-container";
+    category.textContent = image.suggestedCategory;
+    */
+
+    // Append buttons to overlay
+
+    downloadMore.appendChild(downloadBtn);
+    downloadMore.appendChild(moreBtn);
+    
+    catagoryIconsave.appendChild(category);
+    catagoryIconsave.appendChild(categoryicon);
+ 
+  
+    saveDrop.appendChild(catagoryIconsave);
+    saveDrop.appendChild(saveBtn);
+    overlay.appendChild(saveDrop);
+    overlay.appendChild(downloadMore);
+
+    const title = document.createElement("h2");
+    title.textContent = image.title;
+
+    const profileImage = document.createElement("img");
+    profileImage.src = image.imageProfileImage;
+    profileImage.className = "profile-image";
+
+    const profileName = document.createElement("p");
+    profileName.textContent = image.profileName;
+
+    const profileContainer = document.createElement("div");
+    profileContainer.className = "profile-container";
+    profileContainer.appendChild(profileImage);
+    profileContainer.appendChild(profileName);
+    //card.appendChild(category);
+
+    card.appendChild(img);
+    card.appendChild(overlay);
+    card.appendChild(title);
+    card.appendChild(profileContainer);
+
+    container.appendChild(card);
+
+    
+    
+  });
+}
+const filterImages = (searchTerm) => {
+  const lowerCaseSearchTerm = searchTerm.toLowerCase();
+  const filteredImages = imageData.filter((data) => {
+    return (
+      data.title.toLowerCase().includes(lowerCaseSearchTerm) ||
+      data.suggestedCategory.toLowerCase().includes(lowerCaseSearchTerm) ||
+      data.profileName.toLowerCase().includes(lowerCaseSearchTerm)
+    );
+  });
+  createImageCards(filteredImages);
+};
+document.getElementById("search-input").addEventListener("input", (event) => {
+  const searchTerm = event.target.value;
+  console.log('Search term:', searchTerm);
+  filterImages(searchTerm);
+});
+window.addEventListener("DOMContentLoaded", createImageCards);
+
+
+
